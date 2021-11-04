ab621d2afe01e4977ff06400f802406c175fcc8f
Christopher Rabone <christopherrabone1@gmail.com>
2021-11-04 11:29:57 +0000

    got list of names from API and displayed

--- pokemon-website/src/App.js
+++ pokemon-website/src/App.js
@@ -1,21 +1,46 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+
 const Pokedex = require('pokeapi-js-wrapper')
 const P = new Pokedex.Pokedex()
 
 function App() {
-  const interval = {
-    offset: 0,
-    limit: 20
+  const [pokemonList, setPokemonList] = useState([])
+  const [fetched, setFetched] = useState(false)
+  const [pokemonData, setPokemonData] = useState([])
+
+  // const interval = {
+  //   offset: 0,
+  //   limit: 20
+  // }
+
+  let numberList = []
+
+  for (let i = 1; i <= 898; i++) {
+    numberList.push(i)
   }
-  P.getPokemonsList(interval).then(function(response) {
-    console.log(response)
-  })
+
+  useEffect(() => {
+    // P.getPokemonsList(interval).then(function(response) {
+    //   setPokemonList(response)
+    //   setFetched(true)
+    // })
+    P.getPokemonByName(numberList).then(function(response) {
+      setPokemonData(response)
+      setFetched(true)
+    })
+  }, [])
+
+  if (fetched) {
+    console.log(pokemonData[0].name)
+  }
 
   return (
     <div className="page-container">
       <div className="title-container">
         <h1 className="title">Pokédex</h1>
       </div>
+      <div className="content-container">{fetched ? pokemonData.map(el => <p key={el.name}>{el.name}</p>) : ''}</div>
     </div>
   )
 }
